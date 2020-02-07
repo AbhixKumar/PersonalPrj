11b4c8fa42db7860b9c99c310f29a94da036c34b
Abhi Kumar <abhishekumarfvtc@gmail.com>
2020-02-07 00:56:38 -0600

    importing project

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext:IdentityDbContext<VODUser>
+    {
+        DbContextOptions<VODContext> options;
+        
+        public VODContext(DbContextOptions<VODContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
